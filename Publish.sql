16d8affb55a91c3c6cda9fc058ca87e42f0c1a8a
N1i2h3a4 <135349773+N1i2h3a4@users.noreply.github.com>
2023-07-19 15:10:38 -0400

    Publish.sql

--- Publish.sql
+++ Publish.sql
@@ -21,16 +21,20 @@ GROUP BY year, month;
 
 #create a report with top 4 artists with number of movie release per each year?(also consider unique movie names only)
 
-SELECT
-  d.director AS artist,
-  EXTRACT(YEAR FROM m.release_date) AS year,
-  COUNT(DISTINCT m.title) AS movie_count
-FROM m_table m
-JOIN details d ON m.id = d.id
-GROUP BY artist, year
-HAVING COUNT(DISTINCT m.title) > 0
+WITH ranked_artists AS (
+  SELECT
+    d.director AS artist,
+    EXTRACT(YEAR FROM m.release_date) AS year,
+    COUNT(DISTINCT m.title) AS movie_count,
+    ROW_NUMBER() OVER (PARTITION BY EXTRACT(YEAR FROM m.release_date) ORDER BY COUNT(DISTINCT m.title) DESC) AS rn
+  FROM m_table m
+  JOIN details d ON m.id = d.id
+  GROUP BY artist, year
+)
+SELECT artist, year, movie_count
+FROM ranked_artists ra
+WHERE rn <= 4
 ORDER BY year, movie_count DESC;
-
 
 
 #Give me the top 4 month with most number of releases?
