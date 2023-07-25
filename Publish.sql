410bb157b7c0ac105b0798b819acd2cd98edbdde
N1i2h3a4 <135349773+N1i2h3a4@users.noreply.github.com>
2023-07-25 16:10:24 -0400

    Publish.sql

--- Publish.sql
+++ Publish.sql
@@ -1,12 +1,12 @@
 
+
  # Trend of the movie count released in multiple language per month/year
 SELECT
   EXTRACT(YEAR FROM m.release_date) AS year,
   EXTRACT(MONTH FROM m.release_date) AS month,
   COUNT(*) AS movie_count
-FROM m_table m
+FROM ingest.m_table m
 GROUP BY year, month;
-
 
 
 #Trend of the movie count release in english only per month/year
@@ -15,7 +15,7 @@ SELECT
   EXTRACT(YEAR FROM m.release_date) AS year,
   EXTRACT(MONTH FROM m.release_date) AS month,
   COUNT(*) AS movie_count
-FROM m_table m
+FROM ingest.m_table m
 WHERE m.language = 'en'
 GROUP BY year, month;
 
@@ -28,8 +28,8 @@ WITH ranked_artists AS (
     EXTRACT(YEAR FROM m.release_date) AS year,
     COUNT(DISTINCT m.title) AS movie_count,
     ROW_NUMBER() OVER (PARTITION BY EXTRACT(YEAR FROM m.release_date) ORDER BY COUNT(DISTINCT m.title) DESC) AS rn
-  FROM m_table m
-  JOIN details d ON m.id = d.id
+  FROM ingest.m_table m
+  JOIN ingest.details d ON m.id = d.id
   GROUP BY artist, year
 )
 SELECT artist, year, movie_count
@@ -46,7 +46,7 @@ FROM (
     EXTRACT(MONTH FROM m_table.release_date) AS month,
     COUNT(*) AS movie_count,
     ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS row_num
-  FROM m_table
+  FROM ingest.m_table
   GROUP BY month
 ) subquery
 WHERE row_num <= 4;
