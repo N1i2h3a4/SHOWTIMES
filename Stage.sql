845cdf191b50153e0e5629b2080030c246f28e6f
N1i2h3a4 <135349773+N1i2h3a4@users.noreply.github.com>
2023-07-25 11:05:01 -0400

    Stage.sql

--- Stage.sql
+++ Stage.sql
@@ -1,23 +1,24 @@
 create or replace database ingest_data;
 
 use database ingest_data;
+create schema ing;
 
-create table organize_json(json_data VARIANT);
+create or replace table ing.organize_json(json_data VARIANT);
 
 CREATE STAGE my_stage
-URL = 's3://showti/showtimes.json'
+URL = 's3://showti'
 CREDENTIALS = (AWS_KEY_ID = 'AKIAR6U2AZTFW3N2UD5Q' AWS_SECRET_KEY = 'Kz/Rlc4ftUZDWaj38b7Sm/PK8Pb1GOpe/ekN7WEl');
 
 LIST @my_stage;
-
+create or replace file format j_format;
 COPY INTO organize_json
 FROM @my_stage
 FILE_FORMAT = (TYPE = 'JSON');
 
-select * from organize_json;
+select * from ing.organize_json;
 
 
-CREATE OR REPLACE TABLE raw_tab (
+CREATE OR REPLACE TABLE ing.raw_tab (
   j_d VARIANT);
 
 
@@ -26,7 +27,7 @@ SELECT VALUE json_data
 FROM organize_json,
 LATERAL FLATTEN(input => parse_json(organize_json.json_data:results));
 
-select * from raw_tab;
+select * from ing.raw_tab;
 
 
 
